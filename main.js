db1972d1d3d79c670b1be03a297ee9e3ec848c97
Maksim <eaxmple@gmail.com>
2022-10-22 23:24:35 +0200

    Aufgabe1

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 // "use strict";
-let a = 1522332;
-console.log(a);
+const numberOfFilms = +prompt("Wei viel Filmen haben Sie schon gesehen");
+console.log(numberOfFilms + typeof(numberOfFilms));
+
+let personalMovieDB={
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    private: false    
+};
+
+let film1=prompt("Letzte Film");
+let bewertung1=prompt("Ihre bewertung");
+let film2=prompt("Letzte Film");
+let bewertung2=prompt("Ihre bewertung");
+
+personalMovieDB.count=numberOfFilms;
+personalMovieDB.movies[film1]=bewertung1;
+personalMovieDB.movies[film2]=bewertung2;
+console.log(personalMovieDB);
